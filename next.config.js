b2f95c2fb08ef1364769b82ef547b8ac3ad0fb49
Demchenk0 <santadyr2016@gmail.com>
2023-12-12 21:32:41 +0200

    next

--- next.config.js
+++ next.config.js
@@ -2,3 +2,11 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+// next.config.js
+module.exports = {
+    // Інші налаштування...
+    webpack: (config) => {
+      config.resolve.modules.push(__dirname);
+      return config;
+    },
+  };
